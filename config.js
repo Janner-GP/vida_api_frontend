70276b91bd0d4fc9ab4cd475f5d762b1ba530de6
Janner Gonzalez <jannergonpa.10@gmail.com>
2026-03-12 20:47:36 -0500

    Ajustes en las variables para comunicarse con el backend V2

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Simulación de variables de entorno
 const ENV = {
-    API_URL: "https://mahoraga.emausoft.com/api"
+    API_URL: "https://mahoraga.emausoft.com/api/users"
 };
 
 export default ENV;
